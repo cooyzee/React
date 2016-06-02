01a0a083b345ca38e9f1ee00b891cc45c24c2d8b
cooyzee <cooyzee@gmail.com>
2016-06-02 19:12:27 +0800

    June 2 1912

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
 
-gulp.task('default',function(){
+var root = 'app/';
+var paths = {
+    scripts: root+ 'js/**/*.js',
+    sass: root + 'style/app.scss',
+    sassRoot: root + 'style/**/*.scss',
+    images: root + 'img/**/*',
+    dist: root + 'dist'
+};
 
+gulp.task('sass',function(){
+    return sass(paths.sass)
+        .on('error',sass.logError)
+        .pipe(gulp.dest(paths.dist));
 });
+
+gulp.task('watch', function() {
+    gulp.watch(paths.sassRoot, ['sass']);
+});
+
+gulp.task('default',['watch','sass']);
