2ec518411fc8bcc5a7cf79da1f7e0631b4ebe382
cooyzee <cooyzee@gmail.com>
2016-07-29 17:36:00 +0800

    OK, let's call it a day.

--- app/js/other/Other.js
+++ app/js/other/Other.js
@@ -1,6 +1,38 @@
 import React from 'react';
 
 export default class Other extends React.Component{
+    constructor(){
+        super();
+    }
+
+    componentWillMount(){
+
+    }
+
+    componentDidMount(){
+
+    }
+
+    componentWillReceiveProps(){
+
+    }
+
+    shouldComponentUpdate(){
+
+    }
+
+    componentWillUpdate(){
+
+    }
+
+    componentDidUpdate(){
+
+    }
+
+    componentWillUnmount(){
+
+    }
+
     render(){
         return (
             <div>
