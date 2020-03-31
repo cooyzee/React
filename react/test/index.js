5a0253b758dce4f29c098e618cc17d335cedc024
cooyzee <cooyzee@gmail.com>
2020-03-31 18:01:57 +0800

    bootstrap spacing & type

--- react/test/index.js
+++ react/test/index.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 export default function Test() {
   return (
-    <div>
-      <h1>Test</h1>
+    <div className="container-lg">
+      <h1 className="display-1">
+        Test
+      </h1>
+      <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cupiditate eaque molestias neque nihil odio officiis optio quibusdam reprehenderit repudiandae sed sint vel, veritatis. Accusantium quasi quidem reprehenderit ut. Quos.</p>
+      <p><abbr title="HyperText Markup Language" className="initialism">HTML</abbr></p>
+      <blockquote className="blockquote">
+        <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+        <footer className="blockquote-footer">Someone famous in <cite className="mark" title="Source Title">Source Title</cite></footer>
+      </blockquote>
     </div>
   )
 }
